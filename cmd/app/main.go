c2014fc8011b5c24cc7c3fa166ab5b4641e49678
Steve Moyer <smoyer@psu.edu>
2019-02-22 14:02:37 -0500

    Initial set-up to re-create the MDC catalog

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -9,8 +9,17 @@ import (
 
 func main() {
 	dom.Require("https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css")
-	dom.Require("https://fonts.googleapis.com/icon?family=Material+Icons")
+	dom.Require("https://fonts.googleapis.com/icon?family=Material+Icons#.css")
+	dom.Require("https://material-components.github.io/material-components-web-catalog/static/css/main.0729fb5b.css")
 	dom.Require("https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js")
+
+	theme := dom.Doc.CreateElement("meta")
+	theme.SetAttribute("name", "theme-color")
+	theme.SetAttribute("content", "#000000")
+	dom.Head.AppendChild(theme)
+
+	title := dom.Doc.GetElementsByTagName("title")[0]
+	title.SetInnerHTML("Material Components Web - Catalog")
 
 	t := material.NewTopAppBar()
 	dom.Body.AppendChild(t)
